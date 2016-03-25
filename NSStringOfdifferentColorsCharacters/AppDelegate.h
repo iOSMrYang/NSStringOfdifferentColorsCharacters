0f638f6b1dd5fdf84cf68160d8435abc0f6f1d98
杨杰 <yangjie@solot.com>
2016-03-25 16:23:36 +0800

    Initial Commit

---
+++ NSStringOfdifferentColorsCharacters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSStringOfdifferentColorsCharacters
+//
+//  Created by 杨杰 on 16/3/25.
+//  Copyright © 2016年 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSStringOfdifferentColorsCharacters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSStringOfdifferentColorsCharacters
+//
+//  Created by 杨杰 on 16/3/25.
+//  Copyright © 2016年 JY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
